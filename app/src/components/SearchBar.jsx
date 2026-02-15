7c904f2bbb676a19fbcacf86428d5d60753f6d74
joseresu92 <jaresgal@gmail.com>
2026-02-15 13:45:57 +0100

    Fix Search Bar
    Fix Search Bar

--- app/src/components/SearchBar.jsx
+++ app/src/components/SearchBar.jsx
@@ -17,12 +17,39 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="name" placeholder="Name" onChange={handleChange} />
-      <input name="species" placeholder="Species" onChange={handleChange} />
-      <input name="location" placeholder="Location" onChange={handleChange} />
-      <button type="submit">Search</button>
-    </form>
+    <form
+  onSubmit={handleSubmit}
+  className="flex flex-col sm:flex-row items-center gap-3 w-full max-w-3xl mx-auto"
+>
+  <input
+    name="name"
+    placeholder="Nombre"
+    onChange={handleChange}
+    className="flex-1 px-4 py-2 rounded-lg border border-gray-300 bg-white text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition"
+  />
+  
+  <input
+    name="species"
+    placeholder="Especie"
+    onChange={handleChange}
+    className="flex-1 px-4 py-2 rounded-lg border border-gray-300 bg-white text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition"
+  />
+  
+  <input
+    name="location"
+    placeholder="Planeta"
+    onChange={handleChange}
+    className="flex-1 px-4 py-2 rounded-lg border border-gray-300 bg-white text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition"
+  />
+  
+  <button
+    type="submit"
+    className="flex-1 px-4 py-2 rounded-lg  bg-white text-gray-800 "
+  >
+    Search
+  </button>
+</form>
+
   );
 };
 
